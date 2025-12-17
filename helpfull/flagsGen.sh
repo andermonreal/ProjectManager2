a571af9bedcb4406ec4ccf180cdd9bc1c3732fcd
andermonreal <andermonreal2003@gmail.com>
2025-12-17 02:08:37 +0100

    flags

--- helpfull/flagsGen.sh
+++ helpfull/flagsGen.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo "ssi{$(openssl rand -hex 8)}" > /home/www-data/user.txt
-chown www-data:www-data /home/www-data/user.txt
-chmod 600 /home/www-data/user.txt
+echo "ssi{$(openssl rand -hex 8)}" > /home/django/user.txt
+chown django:www-data /home/django/user.txt
+chmod 600 /home/django/user.txt
 
 echo "ssi{$(openssl rand -hex 8)}" > /root/root.txt
 chmod 600 /root/root.txt
-
-gcc -fno-stack-protector -z execstack -o /home/www-data/rootAuth /home/www-data/rootAuth.c
-chmod u+s /home/www-data/rootAuth
