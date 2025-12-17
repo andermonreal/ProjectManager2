75fef7d298195e3979191a85b1d3c6965c55cee1
andermonreal <andermonreal2003@gmail.com>
2025-12-17 04:25:05 +0100

    last commit

--- flagsGen.sh
+++ flagsGen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "ssi{$(openssl rand -hex 8)}" > /home/django/user.txt
+echo "as{$(openssl rand -hex 16)}" > /home/django/user.txt
 chown django:www-data /home/django/user.txt
 chmod 600 /home/django/user.txt
 
-echo "ssi{$(openssl rand -hex 8)}" > /root/root.txt
+echo "as{$(openssl rand -hex 16)}" > /root/root.txt
 chmod 600 /root/root.txt
